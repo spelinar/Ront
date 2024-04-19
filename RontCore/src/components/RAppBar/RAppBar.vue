96a328f15fdc56d67129c9bc0827837afdb72f13
Roman <roman.spelina@actaware.com>
2024-04-19 22:42:43 +0200

    Add RLayout, RAppBar, and RFooter components
    This commit includes the creation of a new main layout named RLayout with corresponding AppBar and Footer components. It also modifies the router file to include the RLayout as a wrapping component for future pages. Additionally, a minor change is made to package.json to rename the project.

---
+++ RontCore/src/components/RAppBar/RAppBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app-bar
+    color="teal-darken-4"
+  >
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+
+</style>
--- RontCore/src/router/index.ts
+++ RontCore/src/router/index.ts
@@ -4,6 +4,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
+    component: () => import('@/layouts/RLayout.vue'),
+    children: [
+      {
+        path: '',
+        name: '',
+        // component: () => import(/* webpackChunkName: "home" */),
+      },
+    ],
   },
 ]
 

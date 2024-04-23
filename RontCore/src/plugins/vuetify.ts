e1e730b922713c39f572a358488019a0ce51e83c
romspe3032 <roman.spelina@pwr.edu.pl>
2024-04-23 11:11:34 +0200

    Update Vuetify plugin configuration
    The configurations for the Vuetify plugin were updated. Custom light and dark themes were added with specified color schemes. Additionally, icon settings were adjusted to use the "fa" set with specific aliases and sets.

--- RontCore/src/plugins/vuetify.ts
+++ RontCore/src/plugins/vuetify.ts
@@ -1,26 +1,48 @@
-/**
- * plugins/vuetify.ts
- *
- * Framework documentation: https://vuetifyjs.com`
- */
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
+import { fa } from "vuetify/iconsets/fa4";
+import { aliases } from "vuetify/iconsets/fa-svg";
 
-// Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+const myCustomLightTheme = {
+  dark: false,
+  colors: {
+    primary: "#b2322b",
+    secondary: "#FDCC99",
+    accent: "#FFFFFF",
+    error: "#b2322b",
+    info: "#428bc1",
+    success: "#529746",
+    warning: "#ff992f",
+  },
+};
 
-// Composables
-import { createVuetify } from 'vuetify'
+const myCustomDarkTheme = {
+  dark: true,
+  colors: {
+    primary: "#b2322b",
+    secondary: "#FDCC99",
+    accent: "#FFFFFF",
+    error: "#b2322b",
+    info: "#428bc1",
+    success: "#529746",
+    warning: "#ff992f",
 
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+  },
+};
+
 export default createVuetify({
-  theme: {
-    themes: {
-      light: {
-        colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
-        },
-      },
+  icons: {
+    defaultSet: "fa",
+    aliases,
+    sets: {
+      fa,
     },
   },
-})
+  theme: {
+    defaultTheme: "light",
+    themes: {
+      light: myCustomLightTheme,
+      dark: myCustomDarkTheme,
+    },
+  },
+});

ad92ce5dc1278ebeaa1e324bd579d35f3431194f
romspe3032 <roman.spelina@pwr.edu.pl>
2024-04-30 15:12:38 +0200

    Create RoAppBarSettings component and update primary color in themes
    Added a new RoAppBarSettings component with Login and Register buttons, and integrated it into RAppBar.vue. The primary color for the custom themes in vuetify.ts has been changed. Moreover, an SVG logo has been embedded in the RAppBar component.

--- RontCore/src/components/RAppBar/RAppBar.vue
+++ RontCore/src/components/RAppBar/RAppBar.vue
@@ -1,11 +1,20 @@
 <template>
   <v-app-bar
-    color="teal-darken-4"
+    elevation="1"
   >
+    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+      <!-- Triangle -->
+      <path d="M 100,10 L 190,190 L 10,190 Z" fill="none" stroke="black"/>
+      <!-- Text -->
+      <text x="50%" y="60%" text-anchor="middle" fill="black" font-size="30" font-family="sans-serif" dy=".3em">Rorent</text>
+    </svg>
+    <v-spacer />
+    <ro-app-bar-settings></ro-app-bar-settings>
   </v-app-bar>
 </template>
 
 <script lang="ts" setup>
+import RoAppBarSettings from "@/components/RoAppBarSettings/RoAppBarSettings.vue";
 </script>
 
 <style scoped>

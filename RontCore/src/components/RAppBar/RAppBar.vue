1843c120a50ac85a0832bce148fc4bd5c306d533
Roman <roman.spelina@actaware.com>
2024-05-02 21:44:49 +0200

    add new logo

--- RontCore/src/components/RAppBar/RAppBar.vue
+++ RontCore/src/components/RAppBar/RAppBar.vue
@@ -2,19 +2,20 @@
   <v-app-bar
     elevation="1"
   >
-    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
-      <!-- Triangle -->
-      <path d="M 100,10 L 190,190 L 10,190 Z" fill="none" stroke="black"/>
-      <!-- Text -->
-      <text x="50%" y="60%" text-anchor="middle" fill="black" font-size="30" font-family="sans-serif" dy=".3em">Rorent</text>
-    </svg>
-    <v-spacer />
+    <v-container>
+      <v-img :src="roLogo" width="200px" />
+    </v-container>
+    <v-spacer/>
     <ro-app-bar-settings></ro-app-bar-settings>
   </v-app-bar>
 </template>
 
 <script lang="ts" setup>
 import RoAppBarSettings from "@/components/RoAppBarSettings/RoAppBarSettings.vue";
+import RoLogo from '@/assets/ro-logo.png'
+
+
+const roLogo = RoLogo;
 </script>
 
 <style scoped>
